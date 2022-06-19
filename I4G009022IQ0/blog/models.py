bd8f8f9fccf3f8687e1490901660b7628d3f4ebb
JASOPEOROLADE <oroladejasope97@gmail.com>
2022-06-19 14:47:47 +0100

    Final submission

---
+++ I4G009022IQ0/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField
+    Author = get_user_model()
+    Created_date = models.DateTimeField
+    Published_date = models.DateTimeField
+
+    def __str__(self) -> str:
+        return self.Title
